3a3a25eb8f0e9afcff1ff61bb4ff7c2ecbf61713
Anton <anton_dv@list.ru>
2018-01-27 10:12:03 +0200

    Add contact and search.

--- src/com/company/Contact.java
+++ src/com/company/Contact.java
@@ -4,4 +4,15 @@ public class Contact {
     String name;
     String email;
     String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Contact(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
 public class Main {
+    private static Contact contact1 = new Contact("Vasya", "+3801234567");
+    private static Contact contact2 = new Contact("Petya", "+3801234568");
+    private static Contact contact3 = new Contact("Kolya", "+3801234569");
+    private static Contact contact4 = new Contact("Ira", "+3801234560");
 
     public static void main(String[] args) {
 	ContactManager myContactManager = new ContactManager();
+	    myContactManager.addContact(contact1);
+        myContactManager.addContact(contact2);
+        myContactManager.addContact(contact3);
+        myContactManager.addContact(contact4);
+
+        Contact cont = myContactManager.searchContact("Vasya");
+        System.out.println(cont.phoneNumber);
     }
+
+
+
+
 }
